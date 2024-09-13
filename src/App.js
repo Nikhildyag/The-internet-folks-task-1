96ff1ec1d9bf7b0c1b668292c82c1f004d4f7ea3
Nikhil Reddy <nikhildyaga123@gmail.com>
2024-09-13 14:15:57 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
+
+const Applayout = () => {
+  return (
+    <div className="App">
+      <Outlet />
+    </div>
+  )
+}
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Applayout />,
+    children: [
+      {
+        path: '/',
+        element: <LandingPage />,
+      },
+    ],
+  },
+])
+function App() {
+  return (
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
+  )
+}
+
+export default App
